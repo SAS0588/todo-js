179160eb6f2c16d0666cd641091e701e87a8a175
Sharieff <TSAS5@unigroupinc.com>
2019-10-07 20:14:47 -0500

    added ability to add new items
    added ability to add new items, however, now the marked complete items disappear and the count is also wrong.

--- script/todo.js
+++ script/todo.js
@@ -1,4 +1,5 @@
-let todos = [{
+let todos = [
+  {
     id: 1,
     text: "Take out trash and recycling",
     complete: false
@@ -29,61 +30,82 @@ let todos = [{
 
 // OBJECTIVE 1 - Display the list of To-Do items
 // Loop through the todo items object
-for (todo in todos) {
+function createList(){
+  removeChild();
+  for (todo in todos) {
 
-  // Create elements
-  const div = document.createElement("div");
-  const input = document.createElement("input");
-  const span = document.createElement("span");
-
-  // Create attributes, assign values, and set to elements
-  const divClass = document.createAttribute("class");
-  const inputClass = document.createAttribute("class");
-  const spanClass = document.createAttribute("class");
-  const inputType = document.createAttribute("type");
-  const spanId = document.createAttribute("id");
-  divClass.value = "todo";
-  inputClass.value = "todo-checkbox";
-  spanClass.value = "todo-text";
-  inputType.value = "checkbox";
-  spanId.value = todos[todo].id;
-  div.setAttributeNode(divClass);
-  input.setAttributeNode(inputClass);
-  input.setAttributeNode(inputType);
-  span.setAttributeNode(spanClass);
-  span.setAttributeNode(spanId);
-
-  // Attach todo list object text to span item
-  const spanItemText = document.createTextNode(todos[todo].text);
-
-  // Add children to parent elements
-  span.appendChild(spanItemText);
-  div.appendChild(input);
-  div.appendChild(span);
-
-  // Attach todo div to parent main div
-  document.getElementById('main-todo-list').appendChild(div);
+    // Create elements
+    const div = document.createElement("div");
+    const input = document.createElement("input");
+    const span = document.createElement("span");
+  
+    // Create attributes, assign values, and set to elements
+    const divClass = document.createAttribute("class");
+    const inputClass = document.createAttribute("class");
+    const spanClass = document.createAttribute("class");
+    const inputType = document.createAttribute("type");
+    const spanId = document.createAttribute("id");
+    divClass.value = "todo";
+    inputClass.value = "todo-checkbox";
+    spanClass.value = "todo-text";
+    inputType.value = "checkbox";
+    spanId.value = todos[todo].id;
+    div.setAttributeNode(divClass);
+    input.setAttributeNode(inputClass);
+    input.setAttributeNode(inputType);
+    span.setAttributeNode(spanClass);
+    span.setAttributeNode(spanId);
+  
+    // Attach todo list object text to span item
+    const spanItemText = document.createTextNode(todos[todo].text);
+  
+    // Add children to parent elements
+    span.appendChild(spanItemText);
+    div.appendChild(input);
+    div.appendChild(span);
+  
+    // Attach todo div to parent main div
+    document.getElementById('main-todo-list').appendChild(div);
+    markComplete();
+  }
 }
+
+// removes all the children in parentNode main-todo-list
+function removeChild(){
+  const mainTodoList = document.getElementById('main-todo-list'); 
+  let child = mainTodoList.lastElementChild;
+  
+  // while condition cycles through each added child and removes it.
+  while (child) { 
+      mainTodoList.removeChild(child);
+      child = mainTodoList.lastElementChild;
+  } 
+}
+
 
 
 // OBJECTIVE 2: Add ability to mark to-do items complete
 // function markComplete() {};=> created to be inserted after textBox creation
 
-const divComplete = document.getElementsByClassName('todo');
-const checkboxes = document.getElementsByClassName('todo-checkbox');
-for (let i = 0; i < checkboxes.length; i++) {
-  checkboxes[i].addEventListener('change', () => {
-    if (checkboxes[i].checked == true) {
-      todos[i].complete = true;
-      divComplete[i].classList.add('complete');
-      updateItems();
-    } else {
-      todos[i].complete = false;
-      divComplete[i].classList.remove('complete');
-      updateItems();
-    };
-  })
-};
+function markComplete(){
+  const divComplete = document.getElementsByClassName('todo');
+  const checkboxes = document.getElementsByClassName('todo-checkbox');
+  for (let i = 0; i < checkboxes.length; i++) {
+    checkboxes[i].addEventListener('change', () => {
+      if (checkboxes[i].checked == true) {
+        todos[i].complete = true;
+        divComplete[i].classList.add('complete');
+        updateItems();
+      } else {
+        todos[i].complete = false;
+        divComplete[i].classList.remove('complete');
+        updateItems();
+      };
+    })
+  };
+}
+
+
 
 // OBJECTIVE 3: Remaining to-do count
 
@@ -98,17 +120,30 @@ let updateItems = () => {
 }
 
 window.onload = function(){
+  createList();
   updateItems();
+
 };
 
-/* Questions for Nick:
-  1. How can my code be written better?
-    info: I feel like my code is very verbose and there are some areas, such as creating spanClass and idClass, and I seem to be violating the DRY principle.
-  2. I tried to do a reduce method, but ultimately failed and retreated to a for loop in updateItems.
-    info: when I tried to do a reduce method, my accumulator was returning a NaN, which for some reason even after debugging was not giving me the result I was looking for, so I switched to a for loop. Is there a way to do this better?
+// OBJECTIVE 4: Create new to-do items
+document.addEventListener('keydown',function(event){
+  if (event.keyCode === 13){
+    grabItem();
+  }   
+});
 
+function grabItem(){
+  const newTodo = document.getElementById("text-input").value;
+  document.getElementById("text-input").value = '';
+  if (newTodo !== ''){
+    addItem(newTodo);
+  } else {
+    console.log("i'm empty");
+  }
+}
 
-
-Once again, thank you Nick for helping me learn how to be a better programmer! I really, truly appreciate it and won't forget your kindness. I will definitely do my best to learn from you and pass on your effort and time to the next person so your teaching can grow.
-
-*/
+function addItem(item){
+  todos.push({id: todos.length + 1, text: item, complete: false});
+  createList();
+  
+}
