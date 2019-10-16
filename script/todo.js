0f33bdd1f34dee4a3138e834729b20f0c5035365
Sharieff <TSAS5@unigroupinc.com>
2019-10-16 08:49:03 -0500

    Finished all objectives of todo app
    Finished all objectives of the todo app. It has full functionality that was required. However, there is a small bug when items are hidden and you enter in a new item. The hidden items do not persist.

--- script/todo.js
+++ script/todo.js
@@ -21,19 +21,33 @@ let todos = [
   }
 ];
 
-// Project plan
-/*
-  Create a controller that calls on separate functions that create the nodes.
 
-  Iterate through the list of objects and put the text in the inner HTML to display to the user.
-*/
 
-// OBJECTIVE 1 - Display the list of To-Do items
-// Loop through the todo items object
-function createList(){
-  removeChild();
+function controller(){
+  loadItems();
+  integrityCheck();
+  updateCount();
+  addItem();
+  markComplete();
+  toggleBtn();
+}
+
+function clearAll(){
+  const mainTodoList = document.getElementById('main-todo-list'); 
+  let child = mainTodoList.lastElementChild;
+  
+  // while condition cycles through each added child and removes it.
+  while (child) { 
+      mainTodoList.removeChild(child);
+      child = mainTodoList.lastElementChild;
+  } 
+}
+
+function loadItems(){
+  //This function loads the object and displays it to the view. 
+
   for (todo in todos) {
-
+  
     // Create elements
     const div = document.createElement("div");
     const input = document.createElement("input");
@@ -42,22 +56,19 @@ function createList(){
     // Create attributes, assign values, and set to elements
     const divClass = document.createAttribute("class");
     const inputClass = document.createAttribute("class");
-    const inputId = document.createAttribute("id");
     const spanClass = document.createAttribute("class");
     const inputType = document.createAttribute("type");
-    const spanId = document.createAttribute("id");
+    const divID = document.createAttribute("id");
     divClass.value = "todo";
     inputClass.value = "todo-checkbox";
-    inputId.value = "check";
     spanClass.value = "todo-text";
     inputType.value = "checkbox";
-    spanId.value = todos[todo].id;
+    divID.value = todos[todo].id;
     div.setAttributeNode(divClass);
+    div.setAttributeNode(divID);
     input.setAttributeNode(inputClass);
-    input.setAttributeNode(inputId);
     input.setAttributeNode(inputType);
     span.setAttributeNode(spanClass);
-    span.setAttributeNode(spanId);
   
     // Attach todo list object text to span item
     const spanItemText = document.createTextNode(todos[todo].text);
@@ -70,89 +81,99 @@ function createList(){
     // Attach todo div to parent main div
     document.getElementById('main-todo-list').appendChild(div);
 
-    // Checks for completed tasks and adds them to the view
-    console.log(todos);
-    if (todos[todo].complete){
-      div.classList.add('complete');
-      document.getElementById('check').click();
-    };
-
   }
 }
 
-
-// removes all the children in parentNode main-todo-list
-function removeChild(){
-  const mainTodoList = document.getElementById('main-todo-list'); 
-  let child = mainTodoList.lastElementChild;
-  
-  // while condition cycles through each added child and removes it.
-  while (child) { 
-      mainTodoList.removeChild(child);
-      child = mainTodoList.lastElementChild;
-  } 
+function addItem(){
+  document.getElementById('text-input').addEventListener('keydown', function (event) {
+    if (event.keyCode === 13) {
+      const newTodo = document.getElementById("text-input").value;
+      document.getElementById("text-input").value = '';
+      if (newTodo !== '') {
+        todos.push({
+          id: todos.length + 1,
+          text: newTodo,
+          complete: false
+        });
+        clearAll();
+        controller();
+      }
+    }
+  });
 }
 
-// OBJECTIVE 2: Add ability to mark to-do items complete
 function markComplete(){
-  const divComplete = document.getElementsByClassName('todo');
-  const checkboxes = document.getElementsByClassName('todo-checkbox');
-  for (let i = 0; i < checkboxes.length; i++) {
-    checkboxes[i].addEventListener('change', () => {
-      if (checkboxes[i].checked == true) {
+  const items = document.getElementsByClassName('todo');
+  let checkbox = document.getElementsByClassName('todo-checkbox');
+  for (let i = 0; i < items.length; i++) {
+    items[i].onclick = function(){
+      if (todos[i].complete === false){
+        checkbox[i].click();
         todos[i].complete = true;
-        divComplete[i].classList.add('complete');
-        updateItems();
-        console.log(todos);
-      } else {
+        items[i].classList.add('complete');
+        updateCount();
+      }
+      else {
+        checkbox[i].click();
         todos[i].complete = false;
-        divComplete[i].classList.remove('complete');
-        updateItems();
-      };
-    })
-  };
-}
-
-
-
-// OBJECTIVE 3: Remaining to-do count
-function updateItems() {
-  let spanCount = 0;
-  for (let i = 0; i < todos.length; i++) {
-    if (todos[i].complete == false) {
-      spanCount++;
+        items[i].classList.remove('complete');
+        updateCount();
+      }
     }
   }
-  document.getElementById('remaining-count').innerHTML = spanCount;
 }
 
-// OBJECTIVE 4: Create new to-do items
-document.addEventListener('keydown', function (event) {
-  if (event.keyCode === 13) {
-    const newTodo = document.getElementById("text-input").value;
-    document.getElementById("text-input").value = '';
-    if (newTodo !== '') {
-      todos.push({
-        id: todos.length + 1,
-        text: newTodo,
-        complete: false
-      });
-      createList();
-      markComplete();
+function integrityCheck(){
+  const itemsIntegrity = document.getElementsByClassName('todo');
+  let checkboxIntegrity = document.getElementsByClassName('todo-checkbox');
+  for (let i = 0; i <todos.length; i++){
+    if (todos[i].complete === true){
+      checkboxIntegrity[i].click();
+      itemsIntegrity[i].classList.add('complete');
+    }
+    else {
+      itemsIntegrity[i].classList.remove('complete');
+    }
+  }
+}
+
+function updateCount(){
+  let count = 0;
+  for (todo of todos){
+    if (todo.complete === false){
+      count++;
+    }
+  }
+  document.getElementById('remaining-count').innerHTML = count;
+}
+
+
+let hidden = false;
+function toggleBtn(){
+  
+  const btnText = document.getElementById('toggle');
+  const divs = document.getElementsByClassName('todo');
+  btnText.onclick = function(){
+    if (hidden === false){
+      btnText.innerHTML = 'Show Completed Items';
+      hidden = true;
+      for (let i = 0; i <= todos.length - 1; i++){
+        if (todos[i].complete === true){
+          divs[i].style.display = 'none';
+        }
+      }
     } else {
-      console.log("i'm empty");
+      btnText.innerHTML = 'Hide Completed Items';
+      hidden = false;
+      for (let i = 0; i <= todos.length - 1; i++){
+        if (todos[i].complete === true){
+          divs[i].style.display = 'block';
+        }
+      }
     }
   }
-});
-
-
-
-
-
-
+}
 
 window.onload = function(){
-  createList();
-  markComplete();
-  updateItems();
-};
+  controller();
+}
